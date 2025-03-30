35b6ac20844eedae9efd210b3cdc45875042d3e3
M0hitMehra <786hitmen@gmail.com>
2025-03-30 20:19:57 +0530

    mohit chnages -1

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Must match your backend
+  baseURL: 'https://meeting-scheduler-backend-dwlu.onrender.com/api', // Must match your backend
 });
 
 API.interceptors.request.use((req) => {
